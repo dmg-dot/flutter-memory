abb3a059caab79f1be2f24604cc5c97bb4ab119e
SID12g <sid12g@naver.com>
2024-01-15 21:02:08 +0900

    feat: attaching a photo (firebase storage)

--- lib/screens/add_memory_screen.dart
+++ lib/screens/add_memory_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -11,7 +12,16 @@ class AddMemoryScreen extends StatefulWidget {
 }
 
 class _AddMemoryScreenState extends State<AddMemoryScreen> {
-  var imageFile;
+  File? imageFile;
+  final storage = FirebaseStorage.instance;
+  Future<void> uploadImage() async {
+    print('전송 시작');
+    final now = DateTime.now(); //현재 시간 저장
+    var ref = storage.ref().child('images/$now.jpg'); //참조 생성
+    ref.putFile(imageFile!); //참조에 파일 저장
+    print('전송 끝!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,13 +74,51 @@ class _AddMemoryScreenState extends State<AddMemoryScreen> {
             Container(
                 margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
                 child: (imageFile != null)
-                    ? Image.file(imageFile, fit: BoxFit.cover)
+                    ? Image.file(imageFile!, fit: BoxFit.cover)
                     : Text('$imageFile')),
             Container(
               margin: const EdgeInsets.fromLTRB(20, 50, 20, 0),
               height: 50,
               child: ElevatedButton(
-                onPressed: () => {},
+                onPressed: () => {
+                  if (imageFile != null)
+                    {
+                      uploadImage(),
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('추억이 등록됐어요'),
+                              content: const Text('추억을 확인해주세요!'),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('알겠어요'))
+                              ],
+                            );
+                          })
+                    }
+                  else
+                    {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('뭔가 누락됐어요!'),
+                              content: const Text('내용을 다시 확인해주세요!'),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('알겠어요'))
+                              ],
+                            );
+                          })
+                    }
+                },
                 child: const Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
